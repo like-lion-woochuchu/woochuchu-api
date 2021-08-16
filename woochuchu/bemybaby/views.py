713354167ba04c45b70c0dfef18909a237b99411
430lyj <430lyj@gmail.com>
2021-08-16 23:30:16 +0900

    refactor : HTTP 코드 수정 등

--- woochuchu/bemybaby/views.py
+++ woochuchu/bemybaby/views.py
@@ -16,10 +16,10 @@ class BeMyBabyFeedView(APIView):
     def get(self, request):
         try :
             bemybaby = self.get_objects()
-            serializer = BeMyBabyFeedSerializer(bemybaby, many=True)
-            return Response(serializer.data, status= status.HTTP_200_OK)
+            serializer = BeMyBabyFeedSerializer(bemybaby, many = True)
+            return Response(serializer.data, status = status.HTTP_200_OK)
         except Exception as e:
-            return Response(status=status.HTTP_404_NOT_FOUND) 
+            return Response(status = status.HTTP_404_NOT_FOUND) 
 
     def post(self, request):
         serializer = BeMyBabyFeedSerializer(data = request.data)
@@ -28,7 +28,7 @@ class BeMyBabyFeedView(APIView):
                 serializer.save()
                 return Response(status = status.HTTP_201_CREATED ) 
         except Exception as e:
-            return Response(status= status.HTTP_400_BAD_REQUEST)
+            return Response(status = status.HTTP_400_BAD_REQUEST)
 
 
 class BeMyBabyFeedDetailView(APIView):
@@ -39,9 +39,9 @@ class BeMyBabyFeedDetailView(APIView):
         try :
             bemybaby = self.get_object(feed_id)
             serializer = BeMyBabyFeedSerializer(bemybaby)
-            return Response(serializer.data, status= status.HTTP_200_OK)
+            return Response(serializer.data, status = status.HTTP_200_OK)
         except Exception as e:
-            return Response(status=status.HTTP_404_NOT_FOUND) 
+            return Response(status = status.HTTP_404_NOT_FOUND) 
 
     def put(self, request, feed_id):
         try :
@@ -49,19 +49,19 @@ class BeMyBabyFeedDetailView(APIView):
             serializer = BeMyBabyFeedSerializer(bemybaby, data = request.data)
             if serializer.is_valid():
                 serializer.save()
-                return Response(status= status.HTTP_200_OK) 
+                return Response(status = status.HTTP_200_OK) 
             else:
                 raise ValueError()
         except Exception as e:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+            return Response(status = status.HTTP_404_NOT_FOUND)
 
     def delete(self, request, feed_id):
         try:
             bemybaby = self.get_object(feed_id)
             bemybaby.delete()
-            return Response(status=status.HTTP_200_OK)
+            return Response(status = status.HTTP_200_OK)
         except Exception as e:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+            return Response(status = status.HTTP_404_NOT_FOUND)
 
 
 class BeMyBabyCommentAPIView(APIView):
@@ -72,17 +72,17 @@ class BeMyBabyCommentAPIView(APIView):
         try:
             comments = self.get_objects(feed_id)
             print(comments)
-            serializer = BeMyBabyCommentSerializer(comments, many=True)
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            serializer = BeMyBabyCommentSerializer(comments, many = True)
+            return Response(serializer.data, status = status.HTTP_200_OK)
         except Exception as e:
-            return Response(status=status.HTTP_200_OK)
+            return Response(status = status.HTTP_200_OK)
     
     def post(self, request, feed_id):
         serializer = BeMyBabyCommentSerializer(data = request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(status = status.HTTP_200_OK ) 
-        return Response(status= status.HTTP_400_BAD_REQUEST)
+        return Response(status = status.HTTP_400_BAD_REQUEST)
 
 class BeMyBabyCommentDetailAPIView(APIView):
     def get_object(self, comment_id):
@@ -92,9 +92,9 @@ class BeMyBabyCommentDetailAPIView(APIView):
         try:
             comment = self.get_object(comment_id)
             serializer = BeMyBabyCommentSerializer(comment)
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.data, status = status.HTTP_200_OK)
         except Exception as e:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+            return Response(status = status.HTTP_404_NOT_FOUND)
 
     def put(self, request, comment_id):
         try:
@@ -106,7 +106,7 @@ class BeMyBabyCommentDetailAPIView(APIView):
             else:
                 raise ValueError()
         except Exception as e:
-            return Response(status= status.HTTP_404_NOT_FOUND)
+            return Response(status = status.HTTP_404_NOT_FOUND)
 
     def delete(self, request, comment_id):
         try:
@@ -114,4 +114,4 @@ class BeMyBabyCommentDetailAPIView(APIView):
             comment.delete()
             return Response(status = status.HTTP_200_OK)
         except Exception as e:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+            return Response(status = status.HTTP_404_NOT_FOUND)
