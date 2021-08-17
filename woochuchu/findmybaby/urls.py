07f8bd85f00f332590569a094cc96515ef4a92e5
SeongEon-Jo <meanaccto@naver.com>
2021-08-17 14:15:00 +0900

    refactor: findmybaby API 틀 변경

--- woochuchu/findmybaby/urls.py
+++ woochuchu/findmybaby/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path, include
 from .views import *
 
-from rest_framework.routers import DefaultRouter
-router = DefaultRouter()
-router.register('', FindMyBabyViewSet)
-
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', FindMyBabyAPIView.as_view()),
     path('<int:feed_id>/', FindMyBabyDeletePutAPIView.as_view()),
     path('<int:feed_id>/comments/', FindMyBabyCommentAPIView.as_view()),
     path('comment/<int:comment_id>/', FindMyBabyCommentDeletePutAPIView.as_view())
