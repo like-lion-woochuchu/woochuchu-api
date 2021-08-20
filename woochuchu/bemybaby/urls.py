c6e08d119905f9cdb0f2e4a532246f70edca6acd
430lyj <430lyj@gmail.com>
2021-08-20 16:57:24 +0900

    refactor : 함수 이름 및 return 값 수정

--- woochuchu/bemybaby/urls.py
+++ woochuchu/bemybaby/urls.py
@@ -5,8 +5,8 @@ from rest_framework.routers import DefaultRouter
 app_name = 'bemybaby'
 
 urlpatterns = [
-    path('', BeMyBabyFeedView.as_view()),
-    path('<int:feed_id>/', BeMyBabyFeedDetailView.as_view()),
+    path('', BeMyBabyAPIView.as_view()),
+    path('<int:feed_id>/', BeMyBabyDeletePutView.as_view()),
     path('<int:feed_id>/comments/', BeMyBabyCommentAPIView.as_view()),
-    path('comment/<int:comment_id>/', BeMyBabyCommentDetailAPIView.as_view()),
+    path('comment/<int:comment_id>/', BeMyBabyCommentDeletePutAPIView.as_view()),
 ]
