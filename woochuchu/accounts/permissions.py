8130b4f9d17f7a55378616cad92ed3700641dcd2
SeongEon-Jo <meanaccto@naver.com>
2021-10-04 21:39:15 +0900

    refactor: requirements 업데이트

--- woochuchu/accounts/permissions.py
+++ woochuchu/accounts/permissions.py
@@ -23,7 +23,7 @@ def get_jwt(request):
         
         return token
     except Exception as e:
-        print(e)
+        # print(e)
         return Exception("Invalid authorization")
 
 class JwtPermission:
