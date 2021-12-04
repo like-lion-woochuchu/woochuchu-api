ec4bb6d420b5a04ebd2d16059b663a9e34bebb95
SeongEon-Jo <meanaccto@naver.com>
2021-12-04 22:16:06 +0900

    refactor: csrf 설정 해체

--- woochuchu/woochuchu/settings/base.py
+++ woochuchu/woochuchu/settings/base.py
@@ -46,7 +46,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
